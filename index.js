a99d145054d562a7af6c22dc07cc74d46146511c
Minionflo <github@minionflo.net>
2021-10-07 00:29:17 +0200

    added days to uptime

--- index.js
+++ index.js
@@ -29,10 +29,11 @@ app.get("/uptime", (req, res) => {
 function sec2time(timeInSeconds) {
     var pad = function(num, size) { return ('000' + num).slice(size * -1); },
     time = parseFloat(timeInSeconds).toFixed(3),
+    days = Math.floor(time / 60 / 60 / 24),
     hours = Math.floor(time / 60 / 60),
     minutes = Math.floor(time / 60) % 60,
     seconds = Math.floor(time - minutes * 60)
-    return pad(hours, 2) + ':' + pad(minutes, 2) + ':' + pad(seconds, 2);
+    return pad(days, 3) + ':' + pad(hours, 2) + ':' + pad(minutes, 2) + ':' + pad(seconds, 2);
 }
 
 
